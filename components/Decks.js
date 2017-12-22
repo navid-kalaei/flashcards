235db3e48422980344156d3ea00b0cada6d629e9
Navid Kalaei <navidkalaei@gmail.com>
2017-12-22 12:53:57 +0330

    feat: add cards to Decks

--- components/Decks.js
+++ components/Decks.js
@@ -1,13 +1,33 @@
 import React, {Component} from 'react'
 import {View, Text} from 'react-native'
+import {Card} from 'react-native-elements'
 
 
 export default class Decks extends Component {
     render() {
         return(
             <View>
-                <Text>Decks</Text>
+                {decks.map((d, i) => (
+                    <Card key={i} title={d.title}>
+                        <Text style={{textAlign:'center'}}>{d.count}</Text>
+                    </Card>
+                ))}
             </View>
         )
     }
 }
+
+const decks = [
+    {
+        title: 'Deck 1',
+        count: 20
+    },
+    {
+        title: 'Deck 2',
+        count: 35
+    },
+    {
+        title: 'Deck 3',
+        count: 45
+    }
+]
