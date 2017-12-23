b30372c10d6489aac7d55bfc8a7e4635fc871326
Navid Kalaei <navidkalaei@gmail.com>
2017-12-23 22:27:31 +0330

    feat: add addCard action

--- actions/decks.js
+++ actions/decks.js
@@ -1,4 +1,4 @@
-import {ADD_DECK, FETCH_DECKS} from './constants'
+import {ADD_CARD, ADD_DECK, FETCH_DECKS} from './constants'
 
 export const addDeck = (title) => {
     return {
@@ -10,4 +10,12 @@ export const addDeck = (title) => {
 export const fetchDecks = (decks) => ({
     type: FETCH_DECKS,
     decks
+})
+
+export const addCard = ({deck, question, answer}) => ({
+    type: ADD_CARD,
+    card: {
+        question,
+        answer
+    }
 })
