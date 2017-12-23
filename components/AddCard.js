675a7efa57525c6fccef6c2e73d07197a41881b4
Navid Kalaei <navidkalaei@gmail.com>
2017-12-23 21:30:28 +0330

    feat: add basic elements to AddCard

--- components/AddCard.js
+++ components/AddCard.js
@@ -3,14 +3,60 @@ import {View, Text} from 'react-native'
 import {FormInput, FormLabel, FormValidationMessage, Button} from 'react-native-elements'
 import {connect} from 'react-redux'
 import * as api from '../utils/api'
-import {NO_INPUT} from '../utils/constants'
 
 
 class AddCard extends Component {
+
+    state = {
+        formError: false,
+        question: '',
+        answer: ''
+    }
+
+    resetState = () => ({
+        formError: false,
+        question: '',
+        answer: ''
+    })
+
+    handleChange = (field) => (input) => (this.setState(() => ({[field]: input})))
+
+    onSubmit = () => {
+        const {question, answer} = this.state
+        if (!question || !answer) {
+            this.setState(() => ({formError: true}))
+        }
+        else {
+            this.resetState()
+            // add card to DB
+            // increment the count
+            // navigate to back
+        }
+    }
+
     render() {
+
+        const {answer, question, formError} = this.state
+
         return(
-            <View>
-                <Text>Add Card!</Text>
+            <View style={{flex: 1}}>
+                <View style={{flex: 1}}>
+                    <FormLabel labelStyle={{fontSize: 17}}>What is the question?</FormLabel>
+                    <FormInput value={question} onChangeText={this.handleChange('question')} maxLength={25}/>
+
+                    <FormLabel labelStyle={{fontSize: 17}}>What is the answer?</FormLabel>
+                    <FormInput value={answer} onChangeText={this.handleChange('answer')} maxLength={25}/>
+
+                    {formError &&
+                    <FormValidationMessage style={{textAlign: 'center'}}>
+                        Question and answer cannot be empty.
+                    </FormValidationMessage>}
+
+                </View>
+
+                <View style={{marginBottom: 24}}>
+                    <Button onPress={this.onSubmit} large title="Add Card"/>
+                </View>
             </View>
         )
     }
