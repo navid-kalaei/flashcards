a0d756726c4d5a7925e822f225890d88777f496e
Navid Kalaei <navidkalaei@gmail.com>
2017-12-23 20:42:57 +0330

    feat: add style to Deck

--- components/Deck.js
+++ components/Deck.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react'
-import {View, Text} from 'react-native'
-
+import {View,StyleSheet} from 'react-native'
+import {Text, Button} from 'react-native-elements'
+import {blue, red} from '../utils/colors'
 
 class Deck extends Component {
 
@@ -14,12 +15,36 @@ class Deck extends Component {
 
     render() {
         return (
-            <View>
-                <Text>Deck</Text>
+            <View style={{flex: 1}}>
+                <View style={styles.content}>
+                    <Text h1 style={styles.text}>Hello</Text>
+                    <Text h4 style={styles.text}>world!</Text>
+                </View>
+                <View style={styles.buttonSection}>
+                    <Button large title='Add Card' backgroundColor={blue} buttonStyle={styles.button}/>
+                    <Button large title='Start Quiz' backgroundColor={red} buttonStyle={styles.button}/>
+                </View>
             </View>
         )
     }
 }
 
+
+const styles = StyleSheet.create({
+    content: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+    text: {
+        textAlign: 'center'
+    },
+    buttonSection: {
+        marginBottom: 16
+    },
+    button: {
+        marginBottom: 8
+    }
+})
 
 export default Deck
