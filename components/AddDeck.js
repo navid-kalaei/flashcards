0259de9455a0fd8f42af2707e999b08f17a20b59
Navid Kalaei <navidkalaei@gmail.com>
2017-12-23 05:26:09 +0330

    feat: add basic elements to AddDeck

--- components/AddDeck.js
+++ components/AddDeck.js
@@ -1,12 +1,39 @@
 import React, {Component} from 'react'
-import {View, Text} from 'react-native'
+import {View} from 'react-native'
+import {FormInput, FormLabel, FormValidationMessage, Button} from 'react-native-elements'
 
 
 export default class AddDeck extends Component {
+
+    state = {
+        isFormValidated: true,
+        title: ''
+    }
+
+    handleChange = (title) => (this.setState(() => ({title})))
+
+    validateForm = (isValid) => (this.setState(() => ({'isFormValidated': isValid})))
+
+    onSubmit = () => {
+        const {title} = this.state
+
+        if (title.length) {
+            this.validateForm(true)
+        }
+        else {
+            this.validateForm(false)
+        }
+    }
+
     render() {
+        const {isFormValidated} = this.state
+
         return(
-            <View>
-                <Text>Add Deck</Text>
+            <View style={{flex: 1}}>
+                <FormLabel labelStyle={{fontSize: 17}}>What is the title of your new deck?</FormLabel>
+                <FormInput onChangeText={this.handleChange} maxLength = {25}/>
+                {!isFormValidated && <FormValidationMessage style={{textAlign: 'center'}}>Title Cannot Be Empty</FormValidationMessage>}
+                <Button onPress={this.onSubmit} large title="Add Deck"/>
             </View>
         )
     }
