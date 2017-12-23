c3837ea01a096662adb5841dfa54f7a46919e5d5
Navid Kalaei <navidkalaei@gmail.com>
2017-12-23 12:44:47 +0330

    feat: create store

---
+++ config/store.js
@@ -0,0 +1,18 @@
+import { createStore, applyMiddleware, compose} from 'redux'
+import logger from 'redux-logger'
+import reducer from '../reducers'
+
+
+const middleware = [logger];
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const store = createStore(
+    reducer,
+    composeEnhancers(
+        applyMiddleware(...middleware)
+    )
+);
+
+
+export default store;
