0ae65a37ffccf2755fed59631ea4719fcb5ab121
Navid Kalaei <navidkalaei@gmail.com>
2017-12-23 13:12:31 +0330

    fix: add api to addDeck action

--- actions/decks.js
+++ actions/decks.js
@@ -1,7 +1,9 @@
 import {ADD_DECK} from './constants'
+import * as api from '../utils/api'
 
-
-export const addDeck = (title) => ({
-    type: ADD_DECK,
-    title
-})
+export const addDeck = (title) => (
+    api.addDeck(title).then({
+        type: ADD_DECK,
+        title
+    })
+)
