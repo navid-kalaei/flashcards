c76f4543b67adb3626dc4d3f75f2b82036ee1781
Navid Kalaei <navidkalaei@gmail.com>
2017-12-25 16:28:59 +0330

    feat: add local notification functions

--- utils/helpers.js
+++ utils/helpers.js
@@ -7,3 +7,53 @@ export const decksToArray = (decks) => (
         return arrayDeck
     }, [])
 )
+
+export const clearLocalNotification = () => (
+    AsyncStorage.removeItem(NOTIFICATION_KEY)
+        .then(Notifications.cancelAllScheduledNotificationsAsync)
+)
+
+export const createNotification = () => ({
+    title: 'Take a quiz!',
+    body: 'Don\'t forget to test yourself today!',
+    ios: {
+        sound: true
+    },
+    android: {
+        sound: true,
+        priority: 'high',
+        sticky: false,
+        vibrate: true
+    }
+})
+
+export const setLocalNotification = () => (
+    AsyncStorage.getItem(NOTIFICATION_KEY)
+        .then(JSON.parse)
+        .then((data) => {
+            if (data === null) {
+                Permissions.askAsync(Permissions.NOTIFICATIONS)
+                    .then(({status}) => {
+                        if (status === 'granted') {
+                            Notifications.cancelAllScheduledNotificationsAsync()
+
+                            let tomorrow = new Date()
+                            tomorrow.setDate(tomorrow.getDate() + 1)
+                            tomorrow.setHours(20)
+                            tomorrow.setMinutes(0)
+
+                            Notifications.scheduleLocalNotificationAsync(
+                                createNotification(),
+                                {
+                                    time: tomorrow,
+                                    repeat: 'day'
+
+                                }
+                            )
+
+                            AsyncStorage.setItem(NOTIFICATION_KEY, JSON.stringify(true))
+                        }
+                    })
+            }
+        })
+)
