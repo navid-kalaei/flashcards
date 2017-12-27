15f474f832b955d78944f5a8dae8114c73d7b34f
Navid Kalaei <navidkalaei@gmail.com>
2017-12-27 18:12:01 +0330

    refactor: separate Tabs navigation

--- App.js
+++ App.js
@@ -1,58 +1,17 @@
 import React, {Component} from 'react'
-import {View, StatusBar, Platform} from 'react-native'
-import {TabNavigator, StackNavigator} from 'react-navigation'
+import {View, StatusBar} from 'react-native'
+import {StackNavigator} from 'react-navigation'
+import Tabs from './routes/Tabs'
 import {Provider} from 'react-redux'
 import store from './config/store'
 import {Constants} from 'expo'
 import {cleanupDb} from './utils/api'
 import {FontAwesome, Ionicons} from '@expo/vector-icons'
 import {purple, white} from './utils/colors'
-import Decks from './components/Decks'
-import AddDeck from './components/AddDeck'
 import Deck from './components/Deck'
 import AddCard from './components/AddCard'
 import Quiz from './components/Quiz'
 import {setLocalNotification} from './utils/helpers'
-
-
-const Tabs = TabNavigator(
-    {
-        Decks: {
-            screen: Decks,
-            navigationOptions: {
-                tabBarLabel: 'Decks',
-                tabBarIcon: ({tintColor}) => <Ionicons name='ios-bookmarks' size={30} color={tintColor}/>
-            }
-        },
-        AddDeck: {
-            screen: AddDeck,
-            navigationOptions: {
-                tabBarLabel: 'New Deck',
-                tabBarIcon: ({tintColor}) => <FontAwesome name='plus-square' size={30} color={tintColor}/>
-            }
-        }
-    },
-    {
-        navigationOptions: {
-            header: null
-        },
-        tabBarOptions: {
-            activeTintColor: Platform.OS === 'ios' ? purple : white,
-            style: {
-                height: 56,
-                backgroundColor: Platform.OS === 'ios' ? white : purple,
-                shadowRadius: 6,
-                shadowOpacity: 1,
-                shadowColor: 'rgba(0,0,0,0.24)',
-                shadowOffset: {
-                    width: 0,
-                    height: 3
-
-                }
-            }
-        }
-    }
-)
 
 
 const MainNavigator = StackNavigator({
---
+++ routes/Tabs.js
@@ -0,0 +1,48 @@
+import {TabNavigator} from 'react-navigation'
+import {purple, white} from '../utils/colors'
+import {Platform} from 'react-native'
+import AddDeck from '../components/AddDeck'
+import Decks from '../components/Decks'
+
+
+const Tabs = TabNavigator(
+    {
+        Decks: {
+            screen: Decks,
+            navigationOptions: {
+                tabBarLabel: 'Decks',
+                tabBarIcon: ({tintColor}) => <Ionicons name='ios-bookmarks' size={30} color={tintColor}/>
+            }
+        },
+        AddDeck: {
+            screen: AddDeck,
+            navigationOptions: {
+                tabBarLabel: 'New Deck',
+                tabBarIcon: ({tintColor}) => <FontAwesome name='plus-square' size={30} color={tintColor}/>
+            }
+        }
+    },
+    {
+        navigationOptions: {
+            header: null
+        },
+        tabBarOptions: {
+            activeTintColor: Platform.OS === 'ios' ? purple : white,
+            style: {
+                height: 56,
+                backgroundColor: Platform.OS === 'ios' ? white : purple,
+                shadowRadius: 6,
+                shadowOpacity: 1,
+                shadowColor: 'rgba(0,0,0,0.24)',
+                shadowOffset: {
+                    width: 0,
+                    height: 3
+
+                }
+            }
+        }
+    }
+)
+
+
+export default Tabs
